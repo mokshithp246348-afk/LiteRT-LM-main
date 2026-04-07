e1b2a32a4d81c15f3b225407408e338de77b6510
mokshithp246348-afk <mokshithp246348@gmail.com>
2026-04-07 16:48:11 +0530

    Remove duplicate

--- stripe-payments.js
+++ stripe-payments.js
@@ -1,2 +1,4 @@
 // Implements the new Stripe Checkout Payment Gateway
 const stripe = require('stripe')('sk_test_123');
+// Implements the new Stripe Checkout Payment Gateway
+const stripe = require('stripe')('sk_test_123');
